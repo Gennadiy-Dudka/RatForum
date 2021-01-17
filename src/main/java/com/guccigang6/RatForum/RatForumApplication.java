dca6d89ecdf26eb043923306a2a58ab7fd736c7e
Hennadii <criosonexus@gmail.com>
2021-01-17 11:54:58 +0200

    'initial commit. Database was connected'

---
+++ src/main/java/com/guccigang6/RatForum/RatForumApplication.java
@@ -0,0 +1,13 @@
+package com.guccigang6.RatForum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatForumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatForumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guccigang6/RatForum/RatForumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guccigang6.RatForum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatForumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
